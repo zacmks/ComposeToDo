de876b184e7156749dd81827802c7cfb859b483a
Isaac Mitsuaki Saito <isaac.saito@santander.com.br>
2021-07-29 03:59:00 -0300

    Added second variant for color

--- app/src/main/java/com/zacmks/composetodo/MainActivity.kt
+++ app/src/main/java/com/zacmks/composetodo/MainActivity.kt
@@ -18,7 +18,7 @@ class MainActivity : ComponentActivity() {
         window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
 
         setContent {
-            ComposeToDoTheme() {
+            ComposeToDoTheme(darkTheme = true, secondVariant = false) {
                 Surface(color = MaterialTheme.colors.background) {
                     TodoScreen(toDoViewModel)
                 }
--- app/src/main/java/com/zacmks/composetodo/ui/theme/Theme.kt
+++ app/src/main/java/com/zacmks/composetodo/ui/theme/Theme.kt
@@ -1,19 +1,32 @@
 package com.zacmks.composetodo.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.Colors
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
+    primary = Blue700Light,
+    primaryVariant = Blue700Dark,
+    secondary = Pink400
+)
+
+private val LightColorPalette = lightColors(
+    primary = Blue700,
+    primaryVariant = Blue700Dark,
+    secondary = Pink400
+)
+
+private val LightColorPaletteSecondVariant = LightColorPalette.copy(
+    primary = Purple500,
     primaryVariant = Purple700,
     secondary = Teal200
 )
 
-private val LightColorPalette = lightColors(
-    primary = Purple500,
+private val DarkColorPaletteSecondVariant = DarkColorPalette.copy(
+    primary = Purple200,
     primaryVariant = Purple700,
     secondary = Teal200
 )
@@ -21,10 +34,17 @@ private val LightColorPalette = lightColors(
 @Composable
 fun ComposeToDoTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
+    secondVariant: Boolean = false,
     content: @Composable() () -> Unit,
 ) {
+    val colors: Colors = if (secondVariant) {
+        if (darkTheme) DarkColorPaletteSecondVariant else LightColorPaletteSecondVariant
+    } else {
+        if (darkTheme) DarkColorPalette else LightColorPalette
+    }
+
     MaterialTheme(
-        colors = if (darkTheme) DarkColorPalette else LightColorPalette,
+        colors = colors,
         typography = Typography,
         shapes = Shapes,
         content = content
