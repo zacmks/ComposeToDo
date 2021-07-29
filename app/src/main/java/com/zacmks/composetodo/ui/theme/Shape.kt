77b896c24dc9f318bc1542ad01369db84608a2c6
Isaac Mitsuaki Saito <isaac.saito@santander.com.br>
2021-07-29 02:11:40 -0300

    Sample Theme, not pretty =/

--- app/src/main/java/com/zacmks/composetodo/ui/theme/Shape.kt
+++ app/src/main/java/com/zacmks/composetodo/ui/theme/Shape.kt
@@ -1,11 +1,12 @@
 package com.zacmks.composetodo.ui.theme
 
+import androidx.compose.foundation.shape.CutCornerShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Shapes
 import androidx.compose.ui.unit.dp
 
 val Shapes = Shapes(
-    small = RoundedCornerShape(4.dp),
-    medium = RoundedCornerShape(4.dp),
-    large = RoundedCornerShape(0.dp)
+    small = CutCornerShape(topStart = 8.dp),
+    medium = CutCornerShape(topStart = 24.dp),
+    large = RoundedCornerShape(8.dp)
 )
--- app/src/main/java/com/zacmks/composetodo/ui/theme/Theme.kt
+++ app/src/main/java/com/zacmks/composetodo/ui/theme/Theme.kt
@@ -16,30 +16,15 @@ private val LightColorPalette = lightColors(
     primary = Purple500,
     primaryVariant = Purple700,
     secondary = Teal200
-
-    /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
 )
 
 @Composable
 fun ComposeToDoTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    content: @Composable() () -> Unit
+    content: @Composable() () -> Unit,
 ) {
-    val colors = if (darkTheme) {
-        DarkColorPalette
-    } else {
-        LightColorPalette
-    }
-
     MaterialTheme(
-        colors = colors,
+        colors = if (darkTheme) DarkColorPalette else LightColorPalette,
         typography = Typography,
         shapes = Shapes,
         content = content
