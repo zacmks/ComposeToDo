17dcadc4177d40a150f0ec2c9e80b7984a1c2262
matheus-miranda <matheus.sm86@gmail.com>
2021-08-01 04:02:58 -0300

    Add TodoItemListScreen

--- app/src/main/java/com/zacmks/composetodo/TodoItemListScreen.kt
+++ app/src/main/java/com/zacmks/composetodo/TodoItemListScreen.kt
@@ -3,48 +3,62 @@ package com.zacmks.composetodo
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Delete
+import androidx.compose.material.icons.rounded.Send
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.LiveData
+import com.zacmks.composetodo.datasource.TodoItem
+import kotlin.reflect.KFunction1
 
 @Composable
-fun ToDoListScreen(
-    items: List<ToDoItem>, selectedItems: List<ToDoItem>,
-    onAddItem: (ToDoItem) -> Unit, onToggleItem: (ToDoItem) -> Unit,
+fun TodoItemListScreen(
+    items: LiveData<List<TodoItem>>, selectedItems: LiveData<List<TodoItem>>?,
+    onAddItem: KFunction1<TodoItem, Unit>, onToggleItem: KFunction1<TodoItem, Unit>,
     onDeleteItems: () -> Unit,
 ) {
+    val itemsList = items.observeAsState(listOf()).value
+    val selectedItemsList = selectedItems?.observeAsState(listOf())?.value
     val (text, onTextChange) = rememberSaveable { mutableStateOf("") }
 
     val submitItem = {
         if (text.isNotBlank()) {
-            onAddItem(ToDoItem(text))
+            onAddItem(TodoItem(text = text, checked = false))
             onTextChange("")
         }
     }
 
     Scaffold(topBar = {
-        val deleteEnabled = selectedItems.isNotEmpty()
+        val selected = !selectedItemsList.isNullOrEmpty()
         TopAppBar(
             title = { Text(stringResource(id = R.string.app_name)) },
             actions = {
                 IconButton(
                     modifier = Modifier.padding(horizontal = 8.dp),
                     onClick = { onDeleteItems() },
-                    enabled = deleteEnabled
+                    enabled = selected
                 ) {
                     Icon(
                         Icons.Rounded.Delete,
                         contentDescription = "Remover items",
                         tint = LocalContentColor.current.copy(
-                            alpha = if (deleteEnabled) 1.0f else 0.38f
+                            alpha = if (selected) 1.0f else 0.38f
                         )
                     )
                 }
@@ -52,12 +66,13 @@ fun ToDoListScreen(
         )
     }) {
         Column(Modifier.fillMaxSize()) {
-            LazyColumn(Modifier
-                .fillMaxWidth()
-                .weight(1.0f)) {
-                items(items = items) { toDoItem ->
-                    val selected = selectedItems.find { it == toDoItem } != null
-                    ToDoRow(toDoItem, selected, onToggleItem)
+            LazyColumn(
+                Modifier
+                    .fillMaxWidth()
+                    .weight(1.0f)
+            ) {
+                items(items = itemsList) { todoItem ->
+                    ToDoRow(todoItem, onToggleItem)
                 }
             }
             ToDoFieldAndButton(text, onTextChange, submitItem)
@@ -68,10 +83,11 @@ fun ToDoListScreen(
 
 @Composable
 fun ToDoRow(
-    toDoItem: ToDoItem,
-    selected: Boolean = false,
-    doToggle: (ToDoItem) -> Unit,
+    toDoItem: TodoItem,
+    doToggle: (TodoItem) -> Unit,
 ) {
+    var selected = toDoItem.checked
+
     Column(modifier = Modifier.fillMaxWidth()) {
         Row(
             modifier = Modifier.padding(12.dp),
@@ -92,14 +108,18 @@ fun ToDoRow(
             Checkbox(
                 modifier = Modifier.padding(horizontal = 8.dp),
                 checked = selected,
-                onCheckedChange = { doToggle(toDoItem) }
+                onCheckedChange = {
+                    selected = it
+                    toDoItem.checked = selected
+                    doToggle(toDoItem)
+                }
             )
         }
         Divider()
     }
 }
 
-/*@OptIn(ExperimentalComposeUiApi::class)
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun ToDoFieldAndButton(
     text: String,
@@ -120,7 +140,8 @@ fun ToDoFieldAndButton(
                 onAddItem()
                 keyboardController?.hide()
             }),
-            modifier = Modifier.weight(1.0f))
+            modifier = Modifier.weight(1.0f)
+        )
         IconButton(
             onClick = onAddItem,
             modifier = Modifier
@@ -130,13 +151,12 @@ fun ToDoFieldAndButton(
             Icon(Icons.Rounded.Send, contentDescription = "Adicionar item")
         }
     }
-}*/
+}
 
-
-/*@Preview(showBackground = true)
+@Preview(showBackground = true)
 @Composable
 fun PreviewToDoRow() {
-    ToDoRow(ToDoItem("Preview of Todo item"), true, {})
+    ToDoRow(TodoItem(text = "Preview of Todo item", checked = true)) {}
 }
 
 @Preview(showBackground = true)
@@ -144,11 +164,3 @@ fun PreviewToDoRow() {
 fun PreviewToDoFieldAndButton() {
     ToDoFieldAndButton("Adding some text", {}, {})
 }
-
-@Preview(showBackground = true)
-@Composable
-fun PreviewToDoListScreen() {
-    val firstItem = ToDoItem("Wash the Car")
-    val secondItem = ToDoItem("Buy Groceries")
-    ToDoListScreen(listOf(firstItem, secondItem), listOf(secondItem), {}, {}, {})
-}*/
